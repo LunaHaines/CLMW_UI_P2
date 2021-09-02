57f1a8c72089b9eb86033e5441cb35d635f1d7da
LunaHaines <nhaines95@gmail.com>
2021-09-02 07:51:13 -0600

    basic home page implemented

---
+++ src/dtos/principal.ts
@@ -0,0 +1,12 @@
+export class Principal {
+
+    id: string;
+    username: string;
+    role: string;
+
+    constructor (id: string, username: string, role: string) {
+        this.id = id;
+        this.username = username;
+        this.role = role;
+    }
+}
