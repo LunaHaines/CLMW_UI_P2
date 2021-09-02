21652dd533444231cd5a06b40aac1f4f172c9314
LunaHaines <nhaines95@gmail.com>
2021-09-02 07:51:13 -0600

    basic home page implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import HomeComponent from './components/HomeComponent';
+import { useState } from 'react';
+import { Principal } from './dtos/principal';
 
 function App() {
+
+  const [authUser, setAuthUser] = useState(undefined as Principal | undefined)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <HomeComponent currentUser={authUser} />
+      </Router>
+    </>
   );
 }
 
