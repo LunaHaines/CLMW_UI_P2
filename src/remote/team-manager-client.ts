92d782ff623e0990ebf9af7aa51e45f00442fc9b
LunaHaines <nhaines95@gmail.com>
2021-09-03 11:00:14 -0600

    messages now set correctly

--- src/remote/team-manager-client.ts
+++ src/remote/team-manager-client.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const teamManagerClient = axios.create({
     // replace this with the deployed URL as needed
-    baseURL: 'http://localhost:8080/TeamManager',
+    baseURL: 'http://localhost:5000/teaManager',
     headers: {
         'Content-Type': 'application/json',
         // store the api-token in local storage, so that it's accessible here
