47d373a5349908d1a7a50d53e79c5f647c6d57f8
LunaHaines <nhaines95@gmail.com>
2021-09-10 10:33:54 -0600

    blank sidebar when no authUser

--- src/components/SidebarComponent.tsx
+++ src/components/SidebarComponent.tsx
@@ -136,7 +136,6 @@ function SidebarComponent(props: ISidebarProps) {
                     </>
                     :
                     <>
-                        <ListItemText primary='Bottom' />
                     </>
 
                     }
