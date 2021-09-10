40b9639d0d10d1a15b4c1425864844fb2d9e4e44
ColbyDub <cduball@outlook.com>
2021-09-09 23:45:53 -0500

    register

---
+++ src/remote/recruiter-service.ts
@@ -0,0 +1,11 @@
+import RegisterRecruiterRequest from "../dtos/register-recruiter-request";
+import { teamManagerClient } from "./team-manager-client";
+
+export const RegisterNewRecruiter = async (newRecruiter: RegisterRecruiterRequest) => {
+
+    let resp = await teamManagerClient.post('/recruiter', newRecruiter);
+
+    if (resp.status >= 400 && resp.status <= 599){
+        throw resp.data;
+    }
+}
