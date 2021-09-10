3d2edd9db44741888c6039491a4eb3fdab2699a6
LunaHaines <nhaines95@gmail.com>
2021-09-10 12:54:02 -0600

    added getExerciseByTarget

--- src/remote/exercise-service.ts
+++ src/remote/exercise-service.ts
@@ -9,3 +9,13 @@ export const getAllExercises = async () => {
 
     return resp.data;
 }
+
+export const getExerciseByTarget = async (target: string) => {
+    let resp = await exerciseDbClient.get(`/exercises/target/${target}`);
+
+    if (resp.status >= 400 && resp.status <= 599) {
+        throw resp.data;
+    }
+
+    return resp.data;
+}
