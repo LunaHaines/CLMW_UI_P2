b72476e80017132a80b49e7c75f192aed7ca323d
mp34829 <55331051+mp34829@users.noreply.github.com>
2021-09-13 08:09:43 -0700

    Coach Dashboard

---
+++ src/dtos/offer.ts
@@ -0,0 +1,9 @@
+export class Offer {
+    coachUsername: string;
+    playerUsername: string;
+
+    constructor(coachUsername: string, playerUsername: string) {
+        this.coachUsername = coachUsername;
+        this.playerUsername = playerUsername;
+    }
+}
