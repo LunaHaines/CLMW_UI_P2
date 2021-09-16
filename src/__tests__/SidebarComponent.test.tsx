3f1acef5f2d6642486bd6fe0ebd5dc182b7a2d5c
LunaHaines <nhaines95@gmail.com>
2021-09-16 09:47:27 -0600

    sidebar rendering tests

--- src/__tests__/SidebarComponent.test.tsx
+++ src/__tests__/SidebarComponent.test.tsx
@@ -21,16 +21,15 @@ describe('SidebarComponent Test Suite', () => {
         // find wrappers for login and register buttons
         let loginWrapper = wrapper.find('div[id="login"]').at(0);
         let registerWrapper = wrapper.find('div[id="register"]').at(0);
-        console.log(loginWrapper.debug());
 
         // expect the buttons we found to be truthy
         expect(loginWrapper.containsMatchingElement(<span className="MuiTypography-root MuiListItemText-primary MuiTypography-body1 MuiTypography-displayBlock">Login</span>)).toBeTruthy();
         expect(registerWrapper.containsMatchingElement(<span className="MuiTypography-root MuiListItemText-primary MuiTypography-body1 MuiTypography-displayBlock">Register</span>)).toBeTruthy();
     })
 
-    it('Players and Logout render when authUser has role of Player', () => {
+    it('Players and Logout render when authUser has role of Recruiter', () => {
         // mock the props
-        let mockAuthUser = new Principal('id', 'username', 'Player');
+        let mockAuthUser = new Principal('id', 'username', 'Recruiter');
         let mockSetAuthUser = jest.fn();
         let mockDrawerOpen = false;
         let mockSetDrawerOpen = jest.fn();
@@ -41,6 +40,45 @@ describe('SidebarComponent Test Suite', () => {
         // find wrappers for important buttons
         let playersWrapper = wrapper.find('div[id="players"]').at(0);
         let logoutWrapper = wrapper.find('div[id="rec-logout"]').at(0);
+
+        expect(playersWrapper.containsMatchingElement(<span className="MuiTypography-root MuiListItemText-primary MuiTypography-body1 MuiTypography-displayBlock">Players</span>)).toBeTruthy();
+        expect(logoutWrapper.containsMatchingElement(<span className="MuiTypography-root MuiListItemText-primary MuiTypography-body1 MuiTypography-displayBlock">Logout</span>)).toBeTruthy();
+
+    })
+
+    it('Offers, Team, Workouts, Profile, and Logout render when authUser has a role of Player', () => {
+        // mock the props
+        let mockAuthUser = new Principal('id', 'username', 'Player');
+        let mockSetAuthUser = jest.fn();
+        let mockDrawerOpen = false;
+        let mockSetDrawerOpen = jest.fn();
+
+        // set up SidebarComponent wrapper
+        const wrapper = mount(<SidebarComponent authUser={mockAuthUser} setAuthUser={mockSetAuthUser} drawerOpen={mockDrawerOpen} setDrawerOpen={mockSetDrawerOpen} />);
+
+        // expect things to be in the component
+        expect(wrapper.containsMatchingElement(<span className="MuiTypography-root MuiListItemText-primary MuiTypography-body1 MuiTypography-displayBlock">Offers</span>)).toBeTruthy();
+        expect(wrapper.containsMatchingElement(<span className="MuiTypography-root MuiListItemText-primary MuiTypography-body1 MuiTypography-displayBlock">Team</span>)).toBeTruthy();
+        expect(wrapper.containsMatchingElement(<span className="MuiTypography-root MuiListItemText-primary MuiTypography-body1 MuiTypography-displayBlock">Workouts</span>)).toBeTruthy();
+        expect(wrapper.containsMatchingElement(<span className="MuiTypography-root MuiListItemText-primary MuiTypography-body1 MuiTypography-displayBlock">Profile</span>)).toBeTruthy();
+        expect(wrapper.containsMatchingElement(<span className="MuiTypography-root MuiListItemText-primary MuiTypography-body1 MuiTypography-displayBlock">Logout</span>)).toBeTruthy();
+    })
+
+    it('team, workouts, players, and logout render when authUser has a role of Coach', () => {
+        // mock the props
+        let mockAuthUser = new Principal('id', 'username', 'Coach');
+        let mockSetAuthUser = jest.fn();
+        let mockDrawerOpen = false;
+        let mockSetDrawerOpen = jest.fn();
+
+        // set up SideBarComponent wrapper
+        const wrapper = mount(<SidebarComponent authUser={mockAuthUser} setAuthUser={mockSetAuthUser} drawerOpen={mockDrawerOpen} setDrawerOpen={mockSetDrawerOpen} />);
+
+        // expect things to be in the component
+        expect(wrapper.containsMatchingElement(<span className="MuiTypography-root MuiListItemText-primary MuiTypography-body1 MuiTypography-displayBlock">Team</span>)).toBeTruthy();
+        expect(wrapper.containsMatchingElement(<span className="MuiTypography-root MuiListItemText-primary MuiTypography-body1 MuiTypography-displayBlock">Workouts</span>)).toBeTruthy();
+        expect(wrapper.containsMatchingElement(<span className="MuiTypography-root MuiListItemText-primary MuiTypography-body1 MuiTypography-displayBlock">Players</span>)).toBeTruthy();
+        expect(wrapper.containsMatchingElement(<span className="MuiTypography-root MuiListItemText-primary MuiTypography-body1 MuiTypography-displayBlock">Logout</span>)).toBeTruthy();
 
     })
 })
