b5d928a35730f50e2227a675d0ffffd6f7299263
LunaHaines <87956273+LunaHaines@users.noreply.github.com>
2021-09-18 07:42:08 -0600

    began recruiterDashboard tests, figured out mock implementation

---
+++ src/__tests__/RecruiterDashboardComponent.test.tsx
@@ -0,0 +1,32 @@
+import { mount, shallow } from 'enzyme';
+import RecruiterDashboard from '../components/RecruiterDashboardComponent';
+import { Player } from '../dtos/player';
+
+let mockPlayer = new Player('name', 'username', ['offer'], ['exercise'], ['completedExercie'], { skill: 'skill', rating: 5})
+import {rateSkill, recruitAllPlayers } from '../remote/player-service';
+jest.mock('../remote/player-service', () => {
+    recruitAllPlayers: jest.fn().mockImplementation(() => {
+        return [mockPlayer]
+    })
+});
+
+describe('RecruiterDashboardComponent Test Suite', () => {
+    
+    afterEach(() => {
+        jest.resetAllMocks();
+    })
+
+    it('RecruiterDashboard renders successfully', () => {
+        // mock the props
+        let mockSetOpenFn = jest.fn();
+        let mockSetMessageFn = jest.fn();
+        let mockSetSeverityFn = jest.fn();
+
+        // set up the wrapper around the component
+        const wrapper = shallow(<RecruiterDashboard setOpen={mockSetOpenFn} setMessage={mockSetMessageFn} setSeverity={mockSetSeverityFn} />)
+
+        expect(wrapper).toBeTruthy();
+
+    })
+
+})
