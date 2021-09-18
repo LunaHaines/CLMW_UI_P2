384f3ae097c7028206fdffbaef049a37c031f6ac
LunaHaines <87956273+LunaHaines@users.noreply.github.com>
2021-09-18 09:09:00 -0600

    props mocked for first test

--- src/__tests__/CoachTeamComponent.test.tsx
+++ src/__tests__/CoachTeamComponent.test.tsx
@@ -1,4 +1,5 @@
 import { mount, shallow } from 'enzyme';
+import CoachTeamComponent from '../components/CoachTeamComponent';
 import { Coach } from '../dtos/coach'
 
 const mockCoach = new Coach('name', 'username', 'team', 'sport', [['player', 'position']]);
@@ -16,7 +17,19 @@ describe('CoachTeamComponent Test Suite', () => {
         jest.resetAllMocks();
     })
 
-    it('', () => {
+    it('CoachTeamComponent renders successfully', () => {
+        // mock the props
+        let mockAuthUser = undefined;
+        let mockErrorOpen = false;
+        let mockErrorOpenFn = jest.fn();
+        let mockErrorMessage = '';
+        let mockErrorMessageFn = jest.fn();
+        let mockErrorSeverity = undefined;
+        let mockErrorSeverityFn = jest.fn();
+
+        const wrapper = shallow(<CoachTeamComponent authUser={mockAuthUser} errorOpen={mockErrorOpen} setErrorOpen={mockErrorOpenFn} errorMessage={mockErrorMessage} setErrorMessage={mockErrorMessageFn} errorSeverity={mockErrorSeverity} setErrorSeverity={mockErrorSeverityFn} />)
+
+        
 
     })
 })
