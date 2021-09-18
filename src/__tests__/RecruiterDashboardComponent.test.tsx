7b11f961845b15c19334724f77b6d7a4c5917a13
LunaHaines <87956273+LunaHaines@users.noreply.github.com>
2021-09-18 08:36:39 -0600

    last tests for recdash, can't test further because of mockImplementation, apparently

--- src/__tests__/RecruiterDashboardComponent.test.tsx
+++ src/__tests__/RecruiterDashboardComponent.test.tsx
@@ -5,8 +5,9 @@ import { Player } from '../dtos/player';
 let mockPlayer = new Player('name', 'username', ['offer'], ['exercise'], ['completedExercie'], [{ skill: 'skill', rating: 5}])
 import {rateSkill, recruitAllPlayers } from '../remote/player-service';
 jest.mock('../remote/player-service', () => {
+    rateSkill: jest.fn().mockImplementation(() => Promise.resolve())
     recruitAllPlayers: jest.fn().mockImplementation(() => {
-        return [mockPlayer]
+        Promise.resolve([mockPlayer])
     })
 });
 
@@ -27,6 +28,38 @@ describe('RecruiterDashboardComponent Test Suite', () => {
 
         expect(wrapper).toBeTruthy();
 
+    })
+
+    it('Dialog is closed when the component rist renders', () => {
+        // mock the props
+        let mockSetOpenFn = jest.fn();
+        let mockSetMessageFn = jest.fn();
+        let mockSetSeverityFn = jest.fn();
+
+        // set up the wrapper around the component
+        const wrapper = shallow(<RecruiterDashboard setOpen={mockSetOpenFn} setMessage={mockSetMessageFn} setSeverity={mockSetSeverityFn} />)
+        
+        let dialogWrapper = wrapper.find('#dialog');
+
+        expect(dialogWrapper.prop('open')).toBeFalsy();
+
+    })
+
+    it('RecruiterDashboard does nothing when rate is clicked with no selected player', () => {
+        // mock the props
+        let mockSetOpenFn = jest.fn();
+        let mockSetMessageFn = jest.fn();
+        let mockSetSeverityFn = jest.fn();
+
+        // set up the wrapper around the component
+        const wrapper = shallow(<RecruiterDashboard setOpen={mockSetOpenFn} setMessage={mockSetMessageFn} setSeverity={mockSetSeverityFn} />)
+        
+        let rateButton = wrapper.find('#rate-skill');
+        rateButton.simulate('click');
+
+        expect(mockSetOpenFn).not.toBeCalled();
+        expect(mockSetMessageFn).not.toBeCalled();
+        expect(mockSetSeverityFn).not.toBeCalled();
     })
 
 })
