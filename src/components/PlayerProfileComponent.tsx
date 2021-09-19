b537c8ac749267d4b69a02130a9bf2dc15df1cc8
willoney2 <62317624+willoney2@users.noreply.github.com>
2021-09-19 16:09:02 -0500

    Show and delete skills and sports (#36)
    * started on adding skills and sports to a player
    
    * show and delete skills sports
    
    * fixed console logs
    
    * merged and rebased

--- src/components/PlayerProfileComponent.tsx
+++ src/components/PlayerProfileComponent.tsx
@@ -1,10 +1,9 @@
-import { createStyles, Input, InputLabel, Theme, makeStyles, Button, MenuItem, Select } from "@material-ui/core";
+import { createStyles, Input, InputLabel, Theme, makeStyles, Button, MenuItem, Select, Grid, Typography } from "@material-ui/core";
 import FormControl from "@material-ui/core/FormControl";
 import { Color } from '@material-ui/lab/Alert';
 import { Principal } from "../dtos/principal";
-import { useState } from 'react';
-import { AddSkill } from "../remote/player-service";
-import { AddSport } from "../remote/player-service";
+import { useState, useEffect } from 'react';
+import { AddSport, DeleteSkill, getAuthorizedPlayer, AddSkill, DeleteSport } from "../remote/player-service";
 import { AddToProfile } from "../dtos/addToProfile";
 
 interface IPlayerProfileProps{
@@ -20,18 +19,29 @@ const useStyles = makeStyles((theme: Theme) =>
             justifyContent: 'center',
             display: 'flex',
             flexWrap: 'wrap',
+            margin: theme.spacing(12),
+            marginBottom: theme.spacing(1),
+            marginTop: theme.spacing(1)
+        },
+            display: {
+            justifyContent: 'center',
+            flexWrap: 'wrap',
             margin: theme.spacing(12)
         },
         addToProfileButton: {
             justifyContent: 'center',
             display: 'flex',
             flexWrap: 'wrap',
-            margin: theme.spacing(12)
+            margin: theme.spacing(12),
+            marginTop: theme.spacing(2),
+            marginBottom: theme.spacing(15),
         }
     })
 )
 
 function PlayerProfileComponent(props: IPlayerProfileProps){
+    const [skills, setSkills] = useState(undefined as {skill: string, rating: number}[] | undefined);
+    const [sports, setSports] = useState(undefined as {sport: string}[] | undefined);
 
     const [formData, setFormData] = useState({
         addedSkill: "",
@@ -56,12 +66,48 @@ function PlayerProfileComponent(props: IPlayerProfileProps){
                     props.setMessage(`${persistedSkill} successfully added`);
                     props.setSeverity('success');
                     props.setOpen(true);
+                    setFormData({
+                        addedSkill: "",
+                        addedSport: ""
+                    })
+                    showSkills();
                 }
             } catch (e: any) {
                 props.setMessage(e.response.data.message);
                 props.setSeverity('error');
                 props.setOpen(true);
             }
+        }
+    }
+
+    let showSkills = async () => {
+        try{
+            if (props.authUser){
+                let selectedPlayer = await getAuthorizedPlayer(props.authUser.username);
+               let skillArr = selectedPlayer.skills;
+                setSkills(skillArr);
+            }
+        }
+        catch(e){
+            console.log(e);
+        }
+
+    }
+
+    let deleteSkill = async (skill: string) => {
+        try {
+            if(props.authUser){
+                let deleteSkill = new AddToProfile(props.authUser.username, skill);
+                await DeleteSkill(deleteSkill);
+                props.setMessage(`Successfully deleted`);
+                props.setSeverity('success');
+                props.setOpen(true);
+                showSkills();
+            }
+        } catch (e: any) {
+            props.setMessage(e.response.data.message);
+            props.setSeverity('error');
+            props.setOpen(true);
         }
     }
 
@@ -78,6 +124,11 @@ function PlayerProfileComponent(props: IPlayerProfileProps){
                     props.setMessage(`${persistedSport} successfully added`);
                     props.setSeverity('success');
                     props.setOpen(true);
+                    showSports();
+                    setFormData({
+                        addedSkill: "",
+                        addedSport: ""
+                    })
                 }
             } catch (e: any) {
                 props.setMessage(e.response.data.message);
@@ -87,22 +138,81 @@ function PlayerProfileComponent(props: IPlayerProfileProps){
         }
     }
 
+    let showSports = async () => {
+        try{
+            if (props.authUser){
+                let selectedPlayer = await getAuthorizedPlayer(props.authUser.username);
+                let sportArr = selectedPlayer.sports;
+                setSports(sportArr);
+            }
+        }
+        catch(e){
+            console.log(e);
+        }
+
+    }
+
+    let deleteSport = async (sport: string) => {
+        try {
+            if(props.authUser){
+                let deleteSport = new AddToProfile(props.authUser.username, sport);
+                await DeleteSport(deleteSport);
+                props.setMessage(`Successfully deleted`);
+                props.setSeverity('success');
+                props.setOpen(true);
+                showSports();
+            }
+        } catch (e: any) {
+            props.setMessage(e.response.data.message);
+            props.setSeverity('error');
+            props.setOpen(true);
+        }
+    }
+
+    useEffect(() => {
+        showSkills();
+        showSports();
+    }, [])
+
     const classes = useStyles();
 
     return (
         <>
-            <div className={classes.root}>
-                <FormControl margin='normal' fullWidth>
-                    <InputLabel htmlFor='addedSkill'>Add Skill</InputLabel>
-                    <Input
-                        onChange={handleChange}
-                        id='addedSkill'
-                        name='addedSkill'
-                        type='text'
-                        placeholder='Enter a Skill to add'
-                    />
-                </FormControl>
-            </div>
+            <Grid container>
+                <Grid item sm={8}>
+                    <div className={classes.root}>
+                        <FormControl margin='normal' fullWidth>
+                            <InputLabel htmlFor='addedSkill'>Add Skill</InputLabel>
+                            <Input
+                                onChange={handleChange}
+                                id='addedSkill'
+                                name='addedSkill'
+                                type='text'
+                                placeholder='Enter a Skill to add'
+                                value={formData.addedSkill}
+                            />
+                        </FormControl>
+                    </div>
+                </Grid>
+                <Grid item sm>
+                    <tbody className={classes.display}>
+                        {skills?.map(
+                        (item:any, index:any) =>
+                        (
+                            <tr key={index}>
+                                <td><Typography variant='h6'>{item.skill}</Typography></td>
+                                <td><Typography variant='h6'>
+                                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</Typography></td>
+                                <td><Typography variant='h6'>{item.rating}</Typography></td>
+                                <td><Typography variant='h6'>&nbsp;&nbsp;&nbsp;&nbsp;</Typography></td>
+                                <td><Button variant='contained' color='secondary' size='small' 
+                                        onClick={() => deleteSkill(item.skill)}>Delete</Button></td>
+                            </tr>
+                        )
+                        )}
+                    </tbody>
+                </Grid>
+            </Grid>
             <Button
                 id='add-skill-button'
                 className={classes.addToProfileButton}
@@ -113,14 +223,17 @@ function PlayerProfileComponent(props: IPlayerProfileProps){
                 size='large'
                 >  Add Skill
             </Button>
-            <div className={classes.root}>
+            <Grid container>
+                <Grid item sm={8}>
+                <div className={classes.root}>
             <FormControl margin="normal" fullWidth>
-                    <InputLabel htmlFor="addedSport">sport</InputLabel>
+                    <InputLabel htmlFor="addedSport">Sport</InputLabel>
                     <Select
                         id="addedSport"
                         name="addedSport"
                         label="Sport"
                         onChange={handleChange}
+                        value={formData.addedSport}
                     >
                         <MenuItem value="Baseball">Baseball</MenuItem>
                         <MenuItem value="Basketball">Basketball</MenuItem>
@@ -131,9 +244,9 @@ function PlayerProfileComponent(props: IPlayerProfileProps){
                         <MenuItem value="Soccer">Football (Soccer)</MenuItem>
                         <MenuItem value="Golf">Golf</MenuItem>
                         <MenuItem value="Gymnastics">Gymnastics</MenuItem>
-                        <MenuItem value="Technical">Hockey</MenuItem>
+                        <MenuItem value="Hockey">Hockey</MenuItem>
                         <MenuItem value="Lacrosse">Lacrosse</MenuItem>
-                        <MenuItem value="Rugbyl">Rugby</MenuItem>
+                        <MenuItem value="Rugby">Rugby</MenuItem>
                         <MenuItem value="Table Tennis">Table Tennis</MenuItem>
                         <MenuItem value="Tennis">Tennis</MenuItem>
                         <MenuItem value="Volleyball">Volleyball</MenuItem>
@@ -141,6 +254,25 @@ function PlayerProfileComponent(props: IPlayerProfileProps){
                     </Select>
                 </FormControl>
             </div>
+                </Grid>
+                <Grid item sm>
+                    <tbody className={classes.display}>
+                        {sports?.map(
+                        (item:any, index:any) =>
+                        (
+                            <tr key={index} >
+                                <td><Typography variant='h6'>{item}</Typography></td>
+                                <td><Typography variant='h6'>
+                                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                    </Typography></td>
+                                <td><Button variant='contained' color='secondary' size='small' 
+                                        onClick={() => deleteSport(item)}>Delete</Button></td>
+                            </tr>
+                        )
+                        )}
+                    </tbody>
+                </Grid>
+            </Grid>
             <Button
                 id='add-sport-button'
                 className={classes.addToProfileButton}
