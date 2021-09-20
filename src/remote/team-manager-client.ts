dff8ef435effe788a167455600af31528c91f56b
LunaHaines <87956273+LunaHaines@users.noreply.github.com>
2021-09-19 20:53:03 -0600

    api base url changed to elastic beanstalk

--- src/remote/team-manager-client.ts
+++ src/remote/team-manager-client.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const teamManagerClient = axios.create({
     // replace this with the deployed URL as needed
-    baseURL: 'http://localhost:5000/teamManager',
+    baseURL: 'http://teammanager-env.eba-hzybpzkp.us-east-2.elasticbeanstalk.com/teamManager',
     headers: {
         'Content-Type': 'application/json',
         // store the api-token in local storage, so that it's accessible here
